0911976445412489e3f5d37ed5c5bcea158802a5
Thirumani-Akshitha <thirumaniakshitha@gmail.com>
2025-07-06 14:14:30 +0530

    Use DATABASE_URL for Render deployment

--- index.js
+++ index.js
@@ -190,7 +190,7 @@ passport.use("google",
   new GoogleStrategy({
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "https://secrets-48ki.onrender.com/auth/google/secrets",
+      callbackURL: "http://localhost:3000/auth/google/secrets",
       userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
     }, async(accessToken,refreshToken,profile,cb)=> {
       try{
